1177b1f3bc52614d3d21910ba9fc970eeb4333ec
Younwoo Lee <leeyw2709@gmail.com>
2022-04-27 16:00:13 +0900

    f/[Add] Received Message Box

--- frontend/App.js
+++ frontend/App.js
@@ -8,6 +8,7 @@ import MessageScreen from './screens/SendMessage';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import ReceivedBox from './screens/ReceivedBox';
 
 const Tabs = createBottomTabNavigator();
 
@@ -23,7 +24,8 @@ const AlarmStack = createNativeStackNavigator();
 function AlarmStackScreen() {
   return (
     <AlarmStack.Navigator screenOptions={{headerShown: false}}>
-      <AlarmStack.Screen name="Alarm" component={AlarmScreen} />
+      <AlarmStack.Screen name="Alarm" component={ReceivedBox} />
+      {/* <AlarmStack.Screen name="Messege" component={AlarmScreen} /> */}
     </AlarmStack.Navigator>
   );
 }
