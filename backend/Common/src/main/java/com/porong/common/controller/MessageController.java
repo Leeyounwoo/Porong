597ccfbff4d1b2a4dc3570eaf0f5daead83313e7
wajabu <suhan9603@naver.com>
2022-04-29 04:31:04 +0900

    b/[update] message controller & service

--- backend/Common/src/main/java/com/porong/common/controller/MessageController.java
+++ backend/Common/src/main/java/com/porong/common/controller/MessageController.java
@@ -27,51 +27,51 @@ public class MessageController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/")
     Long postMessage(RequestCreateMessageDto requestCreateMessageDto) {
-        messageService.createFond(requestCreateMessageDto);
-        return messageId;
+        messageService.postMessage(requestCreateMessageDto);
+        return MessageId;
     }
 
     // 메세지 삭제
     @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/{messageId}")
-    void deleteMessage(RequestFondDto requestFondDto) {
-        messageService.createFond(requestFondDto);
+    void deleteMessage(@PathVariable("messageId") Long messageId) {
+        messageService.deleteMessage(messageId);
     }
 
     // 확인 안한 메세지 모두 조회
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{memberId}/fetchcheckedmessages")
-    List<ResponseMessageDto> fetchLikedRamens(@PathVariable("memberId") Long memberId) {
-        return messageService.fetchLikedRamens(memberId);
+    List<ResponseMessageDto> fetchCheckedMessages(@PathVariable("memberId") Long memberId) {
+        return messageService.fetchCheckedMessages(memberId);
     }
 
     // 단일 메세지 조회
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(HttpStatus.OK) // 여기서 위경도, 시간 검증 추가?
     @PostMapping("/getmessage")
-    List<ResponseMessageDto> postMessage(RequestMessageDto requestMessageDto) {
-        return messageService.createFond(requestMessageDto);
+    ResponseMessageDto getMessage(RequestMessageDto requestMessageDto) {
+        return messageService.getMessage(requestMessageDto);
 
     }
 
     // 전체 메세지 조회(보낸 메세지, 받은 메세지)
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{memberId}/fetchallmessages")
-    List<ResponseMessageDto> fetchLikedRamens(@PathVariable("memberId") Long memberId) {
-        return messageService.fetchLikedRamens(memberId);
+    List<ResponseMessageDto> fetchAllMessages(@PathVariable("memberId") Long memberId) {
+        return messageService.fetchAllMessages(memberId);
     }
 
     // 확인 안한 메세지들 중 가장 빠른 시간 조건 조회
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{memberId}/getrecentmessagetime")
-    LocalDateTime fetchLikedRamens(@PathVariable("memberId") Long memberId) {
-        return messageService.fetchLikedRamens(memberId);
+    LocalDateTime getRecentMessageTime(@PathVariable("memberId") Long memberId) {
+        return messageService.getRecentMessageTime(memberId);
     }
 
     // 해당 멤버와 주고 받은 (확인한? 확인안한것들까지?) 메세지들을 조회
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/fetchmessagesbymember")
-    List<ResponseMessageDto> postMessage(RequestMessageDto requestMessageDto) {
-        return messageService.createFond(requestMessageDto);
+    List<ResponseMessageDto> fetchMessagesByMember(RequestMessageDto requestMessageDto) {
+        return messageService.fetchMessagesByMember(requestMessageDto);
 
     }
 
--- backend/Common/src/main/java/com/porong/common/service/MessageService.java
+++ backend/Common/src/main/java/com/porong/common/service/MessageService.java
@@ -1,10 +1,17 @@
 package com.porong.common.service;
 
+import com.porong.common.dto.RequestCreateMessageDto;
+import com.porong.common.dto.RequestMessageDto;
+import com.porong.common.dto.ResponseMessageDto;
 import com.porong.common.repository.MemberRepository;
 import com.porong.common.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -13,6 +20,72 @@ public class MessageService {
 
     private final MemberRepository memberRepository;
     private final MessageRepository messageRepository;
+
+    @Transactional
+    public Long postMessage(RequestCreateMessageDto requestCreateMessageDto) {
+        Long memberId = requestLikeDto.getMemberId();
+
+        return messageId;
+    }
+
+    @Transactional
+    public void deleteMessage(Long messageId ) {
+        Long memberId = requestLikeDto.getMemberId();
+
+//        return messageId;
+    }
+
+    public List<ResponseMessageDto> fetchCheckedMessages(Long memberId) {
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        if (optionalMember.isEmpty()) {
+            throw new MemberNotFoundException();
+        }
+        Member member = optionalMember.get();
+
+        return messageId;
+    }
+
+    public ResponseMessageDto getMessage(RequestMessageDto requestMessageDto) {
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        if (optionalMember.isEmpty()) {
+            throw new MemberNotFoundException();
+        }
+        Member member = optionalMember.get();
+
+        return messageId;
+    }
+
+    public List<ResponseMessageDto> fetchAllMessages(Long memberId) {
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        if (optionalMember.isEmpty()) {
+            throw new MemberNotFoundException();
+        }
+        Member member = optionalMember.get();
+
+        return messageId;
+    }
+
+    public LocalDateTime getRecentMessageTime(Long memberId) {
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        if (optionalMember.isEmpty()) {
+            throw new MemberNotFoundException();
+        }
+        Member member = optionalMember.get();
+
+        return messageId;
+    }
+
+    public List<ResponseMessageDto> fetchMessagesByMember(RequestMessageDto requestMessageDto) {
+        Optional<Member> optionalMember = memberRepository.findById(memberId); // 멤버 두개 확인
+        if (optionalMember.isEmpty()) {
+            throw new MemberNotFoundException();
+        }
+        Member member = optionalMember.get();
+
+        return messageId;
+    }
+
+
 
 
 
