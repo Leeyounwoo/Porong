3f54c07133705f90405a862a48ef93edde5c8280
ljk1256 <ljk1256@naver.com>
2022-04-28 13:39:42 +0900

    init entity

--- backend/Common/src/main/java/com/porong/common/domain/Member.java
+++ backend/Common/src/main/java/com/porong/common/domain/Member.java
@@ -4,12 +4,29 @@ package com.porong.common.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
-//@Entity
-//@Getter // 추후
-//@Setter // 수정
-//@Table(name = "member")
-//public class Member {
-//}
+@Entity
+@Getter // 추후
+@Setter // 수정
+@Table(name = "member")
+public class Member {
+    @Id
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "member_kakao_id")
+    private Long kakaoId;
+
+    @Column(name = "member_name")
+    private String name;
+
+    @Column(name = "member_phonenumber")
+    private String phoneNumber;
+
+    @OneToMany
+    @Column(name = "member_following_list")
+    private List<Follow> followingList = new ArrayList<>();
+}
