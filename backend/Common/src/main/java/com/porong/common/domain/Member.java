2715d17a9c5ac01b9b8499144f54d68415720a76
ljk1256 <ljk1256@naver.com>
2022-04-28 13:46:32 +0900

    add follower list

--- backend/Common/src/main/java/com/porong/common/domain/Member.java
+++ backend/Common/src/main/java/com/porong/common/domain/Member.java
@@ -20,16 +20,20 @@ public class Member {
     @Column(name = "member_kakao_id")
     private Long kakaoId;
 
-    @Column(name = "member_name")
+    @Column(name = "member_name") // 본명
     private String name;
 
-    @Column(name = "member_phonenumber")
+    @Column(name = "member_phonenumber") // format : 010 - xxxx - xxxx
     private String phoneNumber;
 
-    @Column(name = "member_profile")
+    @Column(name = "member_profile") // 카카오톡 프로필 사진 url
     private String profileUrl;
 
     @OneToMany
-    @Column(name = "member_following_list")
+    @Column(name = "member_following_list") // 현재 내가 추가한 친구 목록
     private List<Follow> followingList = new ArrayList<>();
+
+    @OneToMany
+    @Column(name = "member_follower_list") // 차단 기능을 위해
+    private List<Follow> followerList = new ArrayList<>();
 }
