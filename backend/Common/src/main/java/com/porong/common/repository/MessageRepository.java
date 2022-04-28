2ff87225ee96705cdb657f3a531fbfe479f015d7
wajabu <suhan9603@naver.com>
2022-04-28 19:23:56 +0900

    b/[update] update message repository

--- backend/Common/src/main/java/com/porong/common/repository/MessageRepository.java
+++ backend/Common/src/main/java/com/porong/common/repository/MessageRepository.java
@@ -2,7 +2,14 @@ package com.porong.common.repository;
 
 import com.porong.common.domain.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+
+//    @Query("SELECT m from message m join fetch m.post where c.post.postId=:postId and c.commentId>0 order by c.commentId ASC ")
+//    public List<Message> findByPostId(@Param("messageId") Long messageId);
 
 }
