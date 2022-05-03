3883b4cf699ace2bdca03825013bae64fc137771
Younwoo Lee <leeyw2709@gmail.com>
2022-05-03 16:18:39 +0900

    f/[Feat] Foreground FCM alert

--- frontend/App.js
+++ frontend/App.js
@@ -2,18 +2,33 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import Tabs from './navigation/Tabs';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { root } from './reducer';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {root} from './reducer';
+import messaging from '@react-native-firebase/messaging';
+import {Alert} from 'react-native';
 
 const store = createStore(root);
+messaging().onMessage(async remoteMessage => {
+  // Get the message body
+  let message_body = remoteMessage.notification.body;
+
+  // Get the message title
+  let message_title = remoteMessage.notification.title;
+
+  // Get message image
+  let avatar = remoteMessage.notification.android.imageUrl;
+
+  // Show an alert to the user
+  Alert.alert(message_title, message_body);
+});
 
 const App = () => {
   return (
     <Provider store={store}>
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+      <NavigationContainer>
+        <Tabs />
+      </NavigationContainer>
     </Provider>
   );
 };
