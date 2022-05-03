9ef005d58ffc91e71bd7df650ac97e9d3bd871bd
wajabu <suhan9603@naver.com>
2022-05-03 16:33:23 +0900

    b/[update] message service

--- backend/Common/src/main/java/com/porong/common/dto/RequestMessageDto.java
+++ backend/Common/src/main/java/com/porong/common/dto/RequestMessageDto.java
@@ -1,6 +1,7 @@
 package com.porong.common.dto;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -12,6 +13,8 @@ public class RequestMessageDto implements Serializable {
     private Long messageId;
     private double latitude; // 현재 위치
     private double longitude; // 현재 위치
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime timeNow; // 현재시간
 
 }
