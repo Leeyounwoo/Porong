0a07d925b23961b698cb41c2f1c7aa9fbf71f1e7
김소은 <kimstacy@naver.com>
2022-05-04 19:56:24 +0900

    b/[Update] Common에 kakao login 구현

--- backend/Common/src/main/java/com/porong/common/repository/MemberRepository.java
+++ backend/Common/src/main/java/com/porong/common/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package com.porong.common.repository;
 
 import com.porong.common.domain.Follow;
 import com.porong.common.domain.Member;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +14,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByPhoneNumber(String phoneNumber);
     Member findByMemberId(long memberId);
     Member findByPhoneNumber(String phoneNumber);
+
+    // 2022-05-04 소은 (카카오 아이디로 맴버 찾기 위해 추가)
+    Member findByKakaoId(Long kakaoId);
 }
