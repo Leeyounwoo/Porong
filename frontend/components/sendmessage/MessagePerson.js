53ce2790714ca09e75d7ee270abc392da9d0618e
woosteelz <woosteelz@naver.com>
2022-05-10 20:50:46 +0900

    f/[Add] 친구목록 불러오기 화면구성 완료

--- frontend/components/sendmessage/MessagePerson.js
+++ frontend/components/sendmessage/MessagePerson.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect, useLayoutEffect} from 'react';
 import {
   StyleSheet,
   FlatList,
@@ -7,76 +7,130 @@ import {
   Platform,
   PermissionsAndroid,
   Button,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
 import Contacts from 'react-native-contacts';
-import { useStore } from 'react-redux';
+import {useStore} from 'react-redux';
 import {personContain} from '../../reducer';
-
+import axios from 'axios';
 
 export default function MessagePerson({navigation}) {
-  const [contacts, setContacts] = useState(null);
+  const [contacts, setContacts] = useState([]);
+  const [fetchedItem, setFetchedItem] = useState([]);
 
   const store = useStore();
-  
-  useEffect(() => {
+
+  useLayoutEffect(() => {
     if (Platform.OS === 'android') {
       PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
         title: 'Contacts',
-        message: ' This app would like to see your contacts',
+        message: '마음 사서함이 회원님의 연락처에 접근하려고 합니다.',
       }).then(() => {
         getList();
       });
     } else if (Platform.OS === 'ios') {
-        getList();
+      getList();
     }
-  }, [])
-  
+  }, []);
+
   const getList = () => {
     Contacts.getAll()
       .then(contacts => {
         setContacts(contacts);
-        console.log(contacts);
+        fetch();
       })
       .catch(e => {
         console.log('cannot access');
       });
-  }
+  };
 
   const next = () => {
     store.dispatch(personContain(contacts));
     navigation.navigate('Time');
   };
 
-  const renderItem = ({item}) => (
-    <View style={styles.itemContainer}>
-      <Text style={styles.contactName}>
-        Name: {`${item.givenName} `} {item.familyName}
-      </Text>
-      {item.phoneNumbers.map(phone => (
-        <Text style={styles.phones}>
-          {phone.label} : {phone.number}
-        </Text>
-      ))}
-    </View>
-  );
+  const fetch = () => {
+    const d = [];
+    for (let i = 0; i < contacts.length; i++) {
+      let num = contacts[i].phoneNumbers[0].number;
+      if (num.includes('-')) {
+        num = num.replaceAll('-', '');
+        num = num.replaceAll(' ', '');
+      }
+      d.push(num);
+    }
+    axios({
+      url: 'http://k6c102.p.ssafy.io:8080/v1/member/fetchContact',
+      method: 'post',
+      data: d,
+    })
+      .then(res => {
+        console.log(res);
+        setFetchedItem(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const PhoneBook = ({item}) => {
+    return (
+      <View style={{flex: 1, flexDirection: 'row', margin: 10}}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Image
+            source={require('../../assets/icons/user.png')}
+            style={{height: 40, width: 40}}
+          />
+        </View>
+        <View
+          style={{
+            flex: 2.5,
+            justifyContent: 'center',
+          }}>
+          <Text>{item.displayName}</Text>
+          <Text>{item.phoneNumbers[0].number}</Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <TouchableOpacity
+            style={{
+              width: '80%',
+              height: '70%',
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'green',
+              borderRadius: 10,
+            }}>
+            <Text style={{color: 'white'}}>초대하기</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
 
   return (
-    <View>
-      <View style={styles.container}>
-        <FlatList
-          data={contacts}
-          renderItem={renderItem}
-          numColumns={1}
-          keyExtractor={(item, index) => index}
-        />
-      </View>
-      <View>
-        <Button title="다음" onPress={next}><Text>다음</Text></Button>
-      </View>
+    <View style={{flex: 1}}>
+      <FlatList
+        style={{flex: 1}}
+        data={fetchedItem}
+        renderItem={PhoneBook}
+        keyExtractor={item => item.phoneNumber}
+      />
+      <View style={{flex: 0.2}}></View>
     </View>
-  )
+  );
 }
-
 
 const styles = StyleSheet.create({
   itemContainer: {
@@ -87,63 +141,3 @@ const styles = StyleSheet.create({
     color: 'blue',
   },
 });
-
-
-
-// class ContactList extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       contacts: null,
-//     };
-//   }
-
-//   componentDidMount() {
-//     if (Platform.OS === 'android') {
-//       PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
-//         title: 'Contacts',
-//         message: ' This app would like to see your contacts',
-//       }).then(() => {
-//         this.getList();
-//       });
-//     } else if (Platform.OS === 'ios') {
-//       this.getList();
-//     }
-//   }
-
-//   getList = () => {
-//     Contacts.getAll()
-//       .then(contacts => {
-//         this.setState({contacts});
-//         console.log(contacts);
-//       })
-//       .catch(e => {
-//         console.log('cannot access');
-//       });
-//   };
-
-//   renderItem = ({item}) => (
-//     <View style={styles.itemContainer}>
-//       <Text style={styles.contactName}>
-//         Name: {`${item.givenName} `} {item.familyName}
-//       </Text>
-//       {item.phoneNumbers.map(phone => (
-//         <Text style={styles.phones}>
-//           {phone.label} : {phone.number}
-//         </Text>
-//       ))}
-//     </View>
-//   );
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <FlatList
-//           data={this.state.contacts}
-//           renderItem={this.renderItem}
-//           numColumns={1}
-//           keyExtractor={(item, index) => index}
-//         />
-//       </View>
-//     );
-//   }
-// }
